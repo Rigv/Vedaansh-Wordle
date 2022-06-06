a7f5d9b1a13db5bd133a215e5b71f71fd2ac2f9e
Rigv <101062410+Rigv@users.noreply.github.com>
2022-06-06 13:26:03 +0530

    commit message

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
