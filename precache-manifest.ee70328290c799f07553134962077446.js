cf2392c49f81e212847df11eb0f149cacc1fcfe1
Rigv <101062410+Rigv@users.noreply.github.com>
2022-06-06 12:38:59 +0530

    Updates

---
+++ precache-manifest.ee70328290c799f07553134962077446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbbd1b0d9b2f0e7d4711",
+    "url": "/static/css/main.1d807b04.chunk.css"
+  },
+  {
+    "revision": "bbbd1b0d9b2f0e7d4711",
+    "url": "/static/js/main.0f95fae3.chunk.js"
+  },
+  {
+    "revision": "278e2d9d95f79b1915c3",
+    "url": "/static/js/runtime~main.83ae72db.js"
+  },
+  {
+    "revision": "9ccb3e7e586481a6c764",
+    "url": "/static/js/2.bef00250.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "44a432e2857f632df811dca836916c74",
+    "url": "/static/media/wordle-bank.44a432e2.txt"
+  },
+  {
+    "revision": "c6e9ddca67e6d5bd519d7c76daf3b94d",
+    "url": "/index.html"
+  }
+];
