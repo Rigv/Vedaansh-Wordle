905bacdadc83f9c8dd653d5b2e06086423c3a3ad
Rigv <101062410+Rigv@users.noreply.github.com>
2022-06-06 13:35:50 +0530

    Updates

---
+++ precache-manifest.12e2efedbb5e70528a66f59a15497191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbbd1b0d9b2f0e7d4711",
+    "url": "/Vedaansh-Wordle/static/css/main.1d807b04.chunk.css"
+  },
+  {
+    "revision": "bbbd1b0d9b2f0e7d4711",
+    "url": "/Vedaansh-Wordle/static/js/main.0f95fae3.chunk.js"
+  },
+  {
+    "revision": "cf3568e136d10df58730",
+    "url": "/Vedaansh-Wordle/static/js/runtime~main.fa05cff5.js"
+  },
+  {
+    "revision": "9ccb3e7e586481a6c764",
+    "url": "/Vedaansh-Wordle/static/js/2.bef00250.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/Vedaansh-Wordle/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "44a432e2857f632df811dca836916c74",
+    "url": "/Vedaansh-Wordle/static/media/wordle-bank.44a432e2.txt"
+  },
+  {
+    "revision": "69e296f1f55b65d4f3d65a27309215e5",
+    "url": "/Vedaansh-Wordle/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee70328290c799f07553134962077446.js"
+  "/Vedaansh-Wordle/precache-manifest.12e2efedbb5e70528a66f59a15497191.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Vedaansh-Wordle/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
